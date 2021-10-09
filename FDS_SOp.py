7e3d23fbe3814fdf8b84639426d081fd49f6a137
PRAJWAL PRASHANT VAIDYA <prajwal.p.vaidya@gmail.com>
2021-10-09 13:20:33 +0530

    Update FDS_SOp.py

--- FDS_SOp.py
+++ FDS_SOp.py
@@ -58,21 +58,21 @@ for i in str:
 print("Frequency of words in the String are \n",freq)
 
 
-- Output:
+#Output:
 
-Enter String : Madam wants to meet my Mom and Dad
+#Enter String : Madam wants to meet my Mom and Dad
  
-Longest Word in the String is  madam
+#Longest Word in the String is  madam
  
-Enter a char to be serched : A
-Frequency of  char  is :  5
+#Enter a char to be serched : A
+#Frequency of  char  is :  5
  
-Not a Palindrome.
+#Not a Palindrome.
 
-madam, mom, dad  is/are Palindrome word in the String.
+#madam, mom, dad  is/are Palindrome word in the String.
 
-Enter Substring to be searched : Meet
-Given Substring is present at index  15 
+#Enter Substring to be searched : Meet
+#Given Substring is present at index  15 
 
-Frequency of words in the String are 
- {'m': 6, 'a': 5, 'd': 4, ' ': 7, 'w': 1, 'n': 2, 't': 3, 's': 1, 'o': 2, 'e': 2, 'y': 1}
+#Frequency of words in the String are 
+# {'m': 6, 'a': 5, 'd': 4, ' ': 7, 'w': 1, 'n': 2, 't': 3, 's': 1, 'o': 2, 'e': 2, 'y': 1}
